8a3fec42d8c97c28805b89a71148ff9e2faf703b
yeaeun123 <yeaeun0125@gmail.com>
2024-06-21 13:17:14 +0900

    240621 오전수업

--- myportal/src/main/java/himedia/myportal/controllers/MainController.java
+++ myportal/src/main/java/himedia/myportal/controllers/MainController.java
@@ -1,13 +1,47 @@
 package himedia.myportal.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import himedia.myportal.exceptions.MainControllerException;
 
 @Controller
 public class MainController {
 
 	@RequestMapping({"/","/main"})
-	public String home() {
-		return "/WEB-INF/views/home.jsp";
+	public String main() {
+//		return "/WEB-INF/views/home.jsp";
+		return "home";
 	}
+	
+	@GetMapping("/except")
+	@ResponseBody
+	public String raiseExcept() {
+		// RuntimeException -> 좀 더 구체적인 예외로 전환한 이후 throw 해준다.
+//		throw new RuntimeException("force Exception");
+		throw new MainControllerException("메인 컨트롤러에서 예외가 발생했습니다.");
+	}
+	
+//	@ExceptionHandler(RuntimeException.class)
+//	@ResponseBody
+//	public String handlerControllerException(RuntimeException e) {
+//		return "Exception : " + e.getMessage();
+//	}
+	
+	@ExceptionHandler(MainControllerException.class)
+	public String handleControllerException(MainControllerException e, Model model) {
+		
+		model.addAttribute("name", e.getClass().getSimpleName());
+		model.addAttribute("message", e.getMessage());
+		
+		return "errors/exception";
+		
+	}
+	
+	
+	
 }
